e1285eb9663b24a238dcbd642d09df19afbd1e72
B2C Developer <ravic@biz2credit.com>
2016-11-29 07:47:24 +0000

    Changes made by B2C Developer (ravic@biz2credit.com).

--- BizNativeApp/app/modules/register/register.component.js
+++ BizNativeApp/app/modules/register/register.component.js
@@ -1,21 +1,41 @@
 "use strict";
 var core_1 = require("@angular/core");
-var RegisterComponent = (function () {
-    function RegisterComponent() {
+var page_1 = require("ui/page");
+var observable_1 = require("data/observable");
+var angular_1 = require("nativescript-telerik-ui-pro/sidedrawer/angular");
+var RegisterComponent = (function (_super) {
+    __extends(RegisterComponent, _super);
+    function RegisterComponent(page, _changeDetectionRef) {
+        _super.call(this);
+        this.page = page;
+        this._changeDetectionRef = _changeDetectionRef;
     }
-    RegisterComponent.prototype.registerSubmit = function () {
-        alert("registerSubmit Call");
+    RegisterComponent.prototype.ngAfterViewInit = function () {
+        this.drawer = this.drawerComponent.sideDrawer;
+        this._changeDetectionRef.detectChanges();
     };
+    RegisterComponent.prototype.ngOnInit = function () {
+        this.set("mainContentText", "SideDrawer for NativeScript can be easily setup in the XML definition of your page by defining main- and drawer-content. The component"
+            + " has a default transition and position and also exposes notifications related to changes in its state. Swipe from left to open side drawer.");
+    };
+    RegisterComponent.prototype.openDrawer = function () {
+        this.drawer.showDrawer();
+    };
+    __decorate([
+        core_1.ViewChild(angular_1.RadSideDrawerComponent), 
+        __metadata('design:type', angular_1.RadSideDrawerComponent)
+    ], RegisterComponent.prototype, "drawerComponent", void 0);
     RegisterComponent = __decorate([
         core_1.Component({
             moduleId: module.id,
             selector: "ns-register",
             templateUrl: "register.component.html",
             styleUrls: ["register.component.css"]
-        }), 
-        __metadata('design:paramtypes', [])
+        }),
+        core_1.Injectable(), 
+        __metadata('design:paramtypes', [page_1.Page, core_1.ChangeDetectorRef])
     ], RegisterComponent);
     return RegisterComponent;
-}());
+}(observable_1.Observable));
 exports.RegisterComponent = RegisterComponent;
 //# sourceMappingURL=register.component.js.map
--- BizNativeApp/app/modules/register/register.component.ts
+++ BizNativeApp/app/modules/register/register.component.ts
@@ -1,4 +1,12 @@
-import { Component } from "@angular/core";
+import { Component, ElementRef, ViewChild, Injectable, OnInit, ChangeDetectorRef } from "@angular/core";
+import { View } from "ui/core/view";
+import { RadSideDrawer } from "nativescript-telerik-ui-pro/sidedrawer";
+import { Page } from "ui/page";
+import { ActionItem } from "ui/action-bar";
+import sideDrawerModule = require('nativescript-telerik-ui-pro/sidedrawer');
+import { Observable } from "data/observable";
+import { RadSideDrawerComponent, SideDrawerType } from "nativescript-telerik-ui-pro/sidedrawer/angular";
+
 
 @Component({
   moduleId: module.id,
@@ -7,9 +15,27 @@ import { Component } from "@angular/core";
   styleUrls: ["register.component.css"]
 })
 
-
-export class RegisterComponent {
-    registerSubmit() {
-        alert("registerSubmit Call");
+@Injectable()
+export class RegisterComponent  extends Observable implements OnInit {
+    constructor(private page: Page, private _changeDetectionRef: ChangeDetectorRef) {
+        super();
     }
+
+    @ViewChild(RadSideDrawerComponent) public drawerComponent: RadSideDrawerComponent;
+    private drawer: SideDrawerType;
+
+    ngAfterViewInit() {
+        this.drawer = this.drawerComponent.sideDrawer;
+        this._changeDetectionRef.detectChanges();
+    }
+
+    ngOnInit() {
+        this.set("mainContentText", "SideDrawer for NativeScript can be easily setup in the XML definition of your page by defining main- and drawer-content. The component"
+            + " has a default transition and position and also exposes notifications related to changes in its state. Swipe from left to open side drawer.");
+    }
+
+    public openDrawer() {
+        this.drawer.showDrawer();
+    }
+
 }
