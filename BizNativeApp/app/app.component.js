f11d89cdc2452dc054b794fb8c3ec188f1f06839
B2C Developer <ravic@biz2credit.com>
2016-11-29 06:10:56 +0000

    Changes made by B2C Developer (ravic@biz2credit.com).

--- BizNativeApp/app/app.component.js
+++ BizNativeApp/app/app.component.js
@@ -1,12 +1,14 @@
 "use strict";
 var core_1 = require("@angular/core");
+var router_1 = require("nativescript-angular/router");
 var AppComponent = (function () {
     function AppComponent() {
     }
     AppComponent = __decorate([
         core_1.Component({
             selector: "ns-main",
-            template: "<page-router-outlet></page-router-outlet>"
+            template: "<page-router-outlet></page-router-outlet>",
+            directives: [router_1.NS_ROUTER_DIRECTIVES]
         }), 
         __metadata('design:paramtypes', [])
     ], AppComponent);
--- BizNativeApp/app/modules/home/home.component.js
+++ BizNativeApp/app/modules/home/home.component.js
@@ -1,30 +1,8 @@
 "use strict";
 var core_1 = require("@angular/core");
-var router_1 = require("@angular/router");
 var HomeComponent = (function () {
-    function HomeComponent(router) {
-        this.router = router;
+    function HomeComponent() {
     }
-    HomeComponent.prototype.login = function () {
-        alert("Login Call");
-    };
-    HomeComponent.prototype.forgotPass = function () {
-        prompt({ title: "Forgot Password", message: "Enter the email address you used to register for Groceries to reset your password.",
-            defaultText: "",
-            okButtonText: "Ok",
-            cancelButtonText: "Cancel"
-        }).then(function (data) {
-            if (data.result) {
-                alert("Forgot Password");
-            }
-        });
-    };
-    HomeComponent.prototype.registerPage = function () {
-        this.router.navigate(["register"]);
-    };
-    HomeComponent.prototype.testcall = function () {
-        alert("sfsfsf");
-    };
     HomeComponent = __decorate([
         core_1.Component({
             moduleId: module.id,
@@ -32,7 +10,7 @@ var HomeComponent = (function () {
             templateUrl: "home.component.html",
             styleUrls: ["home.component.css"]
         }), 
-        __metadata('design:paramtypes', [router_1.Router])
+        __metadata('design:paramtypes', [])
     ], HomeComponent);
     return HomeComponent;
 }());
